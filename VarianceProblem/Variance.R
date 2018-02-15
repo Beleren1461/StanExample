a88138e44549ec4396313840ad138460b69f9fcb
Ettore Riva <jacebeleren1461@gmail.com>
2018-02-15 22:53:46 +0100

    write the problem

--- VarianceProblem/Variance.R
+++ VarianceProblem/Variance.R
@@ -2,19 +2,27 @@ library(rstan)
 library(shinystan)
 
 theta_0=23 # True mean of generator process
-sd_0=10 # True standard deviation of generator process
+sigma.T=5 # True standard deviation of generator process
 
-Y <- rnorm(40,theta_0,sd_0) # generate the data
+sigma_0 <- 100 # stadard deviation of prior distribution
 
-# url of the model
-model.url <- ""
+
+Y <- rnorm(40,theta_0,sigma.T) # generate the data
+N=length(Y)
+
+
+# url of the model 
+model.url <- "https://raw.githubusercontent.com/Beleren1461/StanExample/Variance_Problem/VarianceProblem/Modello.Stan"
 
 # compile the model
 modello.compiled <- rstan::stan_model(file = model.url,model_name = "Test")
 
 # run the sampler
 Stan.sample <- rstan::sampling(modello.compiled,
-                               data=list(Y=Y,N=length(Y)),
+                               data=list(Y=Y,
+                                         N=N,
+                                         sigma_0=sigma_0,
+                                         sigma=sigma.T),
                                iter=1000,
                                warmup=100,
                                algorithm="NUTS")
@@ -23,9 +31,11 @@ Stan.sample <- rstan::sampling(modello.compiled,
 # extract a list of values
 Stan.values <- extract(Stan.sample)
 # puth them into dataframe
-esempio <- data.frame(theta=Stan.values[["theta"]],logPos=Stan.values[["lp__"]])
+example <- data.frame(theta=Stan.values[["theta"]],logPos=Stan.values[["lp__"]])
 
+Sample.sd <- sd(example$theta)
 
-##### result of the book: First 
+##### result https://en.wikipedia.org/wiki/Conjugate_prior 
 
+Posterior.sd <- sqrt((1/sigma_0^2+N/sigma.T^2)^-1)
 
