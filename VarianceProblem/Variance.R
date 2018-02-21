202b6f6193eaf883b715b6341b961032b6b4be19
Ettore Riva <jacebeleren1461@gmail.com>
2018-02-22 00:38:43 +0100

    create graph to show the posterior distribution

--- VarianceProblem/Variance.R
+++ VarianceProblem/Variance.R
@@ -1,5 +1,7 @@
 library(rstan)
 library(shinystan)
+library(ggplot2)
+library(latex2exp)
 
 theta_0=23 # True mean of generator process
 sigma.T=10 # True standard deviation of generator process
@@ -23,8 +25,8 @@ Stan.sample <- rstan::sampling(modello.compiled,
                                          N=N,
                                          sigma_0=sigma_0,
                                          sigma=sigma.T),
-                               iter=1000,
-                               warmup=100,
+                               iter=100000,
+                               warmup=1000,
                                algorithm="NUTS")
 
 
@@ -34,9 +36,15 @@ Stan.values <- extract(Stan.sample)
 example <- data.frame(theta=Stan.values[["theta"]],logPos=Stan.values[["lp__"]])
 
 Sample.sd <- sd(example$theta)
+Sample.mean <- mean(example$theta)
 
 ##### result https://en.wikipedia.org/wiki/Conjugate_prior 
 
 Posterior.sd <- sqrt((1/sigma_0^2+N/sigma.T^2)^-1)
 Posterior.mean <- Posterior.sd^2*(0/sigma_0^2+sum(Y)/sigma.T^2)
 
+gg <- ggplot(data = example,aes(x=theta))
+gg+
+  stat_function(fun = dnorm,args = list(mean = Posterior.mean,sd = Posterior.sd),colour="red",size=1.5)+ 
+  labs(x=TeX("$\\theta$"),y=TeX("$\\pi(\\theta | Y)$")) + 
+  geom_histogram(aes(y=(..count../sum(..count..))*1),bins=16,alpha=0.7)
